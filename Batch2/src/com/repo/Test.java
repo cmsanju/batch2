e42bd9d00ccc793934e3d82e88481d2c795fc000
gonesanjeeva <gonesanjeeva@localhost>
2021-03-08 11:59:00 +0530

    added for loop

--- Batch2/src/com/repo/Test.java
+++ Batch2/src/com/repo/Test.java
@@ -4,5 +4,12 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("git test");
+		
+		System.out.println("hello");
+		
+		for (int i = 0; i < args.length; i++) {
+			
+		}
+		//ieueueu
 	}
 }
